72cc5b80f5af148d0053e782fe8b3758f6158d1f
Jordi Ortiz <jordi.ortiz@um.es>
2017-05-08 20:10:35 +0200

    Introduce onos basic authentication in plugin

--- plugins/cdnapp.py
+++ plugins/cdnapp.py
@@ -1,10 +1,17 @@
 import json, httplib
+import base64
+username = 'karaf'
+password = 'karaf'
 
 def get(controller, prxid, resource, flow):
     host, uri = resource
     body = { 'proxy': prxid, 'hostname': host, 'uri': uri, 'flow': flow }
-    
+
+    auth = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+
     conn = httplib.HTTPConnection(controller)
+    conn.putheader("Authorization", "Basic %s" % auth)
+    conn.endheaders()
     conn.request('POST', "/wm/cdnmanager/priv/v1.0/proxyreq",
         json.dumps(body), { 'content-type': 'application/json' })
     conn.getresponse()
