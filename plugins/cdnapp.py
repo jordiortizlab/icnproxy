09a69fa83c80d8f912372b77e9122ed0b18e7e9d
Jordi Ortiz <jordi.ortiz@um.es>
2017-05-09 09:34:03 +0200

    Change to httplib.HTTP to use extra headers

--- plugins/cdnapp.py
+++ plugins/cdnapp.py
@@ -9,9 +9,15 @@ def get(controller, prxid, resource, flow):
 
     auth = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
 
-    conn = httplib.HTTPConnection(controller)
+    conn = httplib.HTTP(controller)
+    conn.putrequest('POST', "/onos/icn/proxyrequest")
+
+    message = json.dumps(body)
+    conn.putheader('content-type', 'application/json')
+    conn.putheader("Content-length", "%d" % len(message))
     conn.putheader("Authorization", "Basic %s" % auth)
     conn.endheaders()
-    conn.request('POST', "/onos/icn/proxyrequest",
-        json.dumps(body), { 'content-type': 'application/json' })
-    conn.getresponse()
+    conn.send(message)
+    # conn.request('POST', "/onos/icn/proxyrequest",
+    #     json.dumps(body), { 'content-type': 'application/json' })
+    conn.getreply()
