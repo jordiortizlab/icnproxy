9541c0f9b24983824d1a74a4dd79d1973fd0f66a
Jordi Ortiz <jordi.ortiz@um.es>
2017-11-15 08:52:42 +0100

    Adapted launcher to tornado implementation

--- launchproxy.sh
+++ launchproxy.sh
@@ -5,10 +5,9 @@ then
     echo "Virtual Env missing. Creating ./venv/"
     virtualenv -p python3.6 venv
     source venv/bin/activate
-    pip install gunicorn
-    pip install falcon
+    pip install tornado
 else
     source venv/bin/activate
 fi
 
-gunicorn --reload --bind 0.0.0.0:8080 icnproxy
+python icnproxy.py
