b1d78430c121c8868c259858abdbb91a6140707f
Jordi Ortiz <jordi.ortiz@um.es>
2017-12-15 20:59:20 +0100

    Make sure proxy is comletely stopped

--- stopproxy.sh
+++ stopproxy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-for pid in `ps -e -o pid,command  | grep icnproxy.py | grep -v grep | awk '{ print $1 }'`
+let done=1
+while [ $done -eq 1 ]
 do
-    echo killing $pid
-    kill -2 $pid
+    let done=0
+    for pid in `ps -e -o pid,command  | grep icnproxy.py | grep -v grep | awk '{ print $1 }'`
+    do
+	echo killing $pid
+	kill -2 $pid
+	let done=1
+    done
+
 done
